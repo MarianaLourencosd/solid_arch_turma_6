8642a786376010e118c97732a0e24c06b9104b07
Mariana Lourenço <marianalourencosd@gmail.com>
2026-04-22 12:47:40 -0300

    JWT implentação token inicial

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,39 +1,41 @@
+const createUserToken = require('../helpers/create-user-token')
 const User = require('../models/User')
-const bcrypt = require ('bcrypt')
+const bcrypt = require('bcrypt')
 
 module.exports = class UserController {
     static async register(req, res) {
-        const {name,email,phone,password,confirmpassword} = req.body
-        
-        if(!name){
-        res.status(422).json({message: 'Nome é obrigatorio'})
-        return
-        }
-        if(!email){
-        res.status(422).json({message: 'email é obrigatorio'})
-        return
-        }
-        if(!phone){
-        res.status(422).json({message: 'telefone é obrigatorio'})
-        return
-        }
-        if(!password){
-        res.status(422).json({message: 'Confirmação de senha é obrigatorio'})
-        return
-        }
-        if(password !== confirmpassword){
-        res.status(422).json({message: 'As senhas não coincidem'})
-        return
-        }
-        const userExists = await User.findOne({email: email})
 
-        if(userExists){
-            res.status(422).json({message: 'O usuario ja existe em nossos registros.'})
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        if (!name) {
+            res.status(422).json({ message: 'Nome é obrigatorio' })
+            return
+        }
+        if (!email) {
+            res.status(422).json({ message: 'email é obrigatorio' })
+            return
+        }
+        if (!phone) {
+            res.status(422).json({ message: 'telefone é obrigatorio' })
+            return
+        }
+        if (!password) {
+            res.status(422).json({ message: 'Confirmação de senha é obrigatorio' })
+            return
+        }
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'As senhas não coincidem' })
+            return
+        }
+        const userExists = await User.findOne({ email: email })
+
+        if (userExists) {
+            res.status(422).json({ message: 'O usuario ja existe em nossos registros.' })
             return
         }
 
         const salt = await bcrypt.genSalt(12)
-        const passwordHash = await bcrypt.hash(password,salt)
+        const passwordHash = await bcrypt.hash(password, salt)
 
         const user = new User({
             name,
@@ -41,14 +43,11 @@ module.exports = class UserController {
             phone,
             password: passwordHash,
         })
-
-        try{
+        try {
             const newUser = await user.save()
-            res.status(201).json({message: 'Usuario criado no Get Pet',
-            newUser
-        })
-        } catch (error){
-            res.status(503).json({message: error})
+            await createUserToken(newUser, req,res)
+        } catch (error) {
+            res.status(503).json({ message: error })
         }
     }
 }
